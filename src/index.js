269be7bbe43642cf684d2a9f4024d9843a634010
Michael Salaverry <michaels@wix.com>
2018-06-01 12:57:58 +0300

    added static render on build

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-snapshot';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
 registerServiceWorker();
