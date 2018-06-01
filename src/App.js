97e44f01b741c11799bfdc49b75c142fe78955c9
Michael Salaverry <michaels@wix.com>
2018-06-01 12:58:38 +0300

    cleaned up App.js & App.css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Gallery } from './Gallery';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to a Performant Photo Gallery</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <Gallery />
+        </main>
       </div>
     );
   }
